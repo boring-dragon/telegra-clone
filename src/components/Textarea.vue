59a2d6f0935a0f5afb86794b411f79a51cdbdc4c
Mohamed jinas <j@live.mv>
2022-06-08 08:03:32 +0500

    init

---
+++ src/components/Textarea.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const props = defineProps({
+  modelValue: {
+    required: true,
+  },
+  placeholder: {
+    type: String,
+    default: 'eyo write something..',
+  },
+})
+
+function resize(e) {
+  e.target.style.height = 'auto'
+  e.target.style.height = `${e.target.scrollHeight}px`
+}
+</script>
+
+<template>
+  <textarea class="w-full border-none focus:outline-0 resize-none leading-[34px] overflow-hidden text-[32px]" :placeholder="props.placeholder" :value="props.modelValue" @input="$emit('update:modelValue', $event.target.value); resize($event)" />
+</template>
---
+++ src/pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const state = reactive({
+  title: 'sdfdsf',
+  author: 'jinas',
+})
+</script>
+
+<template>
+  <div class="container mx-auto max-w-3xl">
+    <Textarea v-model="state.title" placeholder="Title" />
+  </div>
+</template>
